2bf86483665b554b8d3a50166bb15a21c1eccb24
Mohammed <allawi465@gmail.com>
2022-11-05 03:32:25 +0100

    unit test done for create post function

--- src/js/api/posts/create.test.js
+++ src/js/api/posts/create.test.js
@@ -1,4 +1,48 @@
 import { createPost } from './create';
-import { LocalStorageMock } from '../../test/LocalStorageMock.js';
 
-global.localStorage = new LocalStorageMock();
+function fetchSuccessPost() {
+  return Promise.resolve({
+    ok: true,
+    status: 200,
+    statusText: 'OK',
+    json: () => Promise.resolve(newPost),
+  });
+}
+
+function fetchFailureLogin(status = 401, statusText = 'refresh token missing') {
+  return Promise.resolve({
+    ok: false,
+    status: 'Unauthorized',
+    statusText,
+  });
+}
+
+const oldPost = { title: 'old Post', body: 'old post body', media: 'url' };
+const newPost = {
+  title: 'new Post',
+  body: 'new post body',
+  media: 'url',
+  id: 1,
+};
+
+describe('createPost', () => {
+  it('The create item function trow error if fetch is a failure', async () => {
+    global.fetch = jest.fn(() => fetchFailureLogin());
+    const creatingNewPost = await createPost(
+      oldPost.title,
+      oldPost.body,
+      oldPost.media
+    );
+    expect(creatingNewPost).toThrow('refresh token missing');
+  });
+
+  it('The create item function creates a new item on the API', async () => {
+    global.fetch = jest.fn(() => fetchSuccessPost());
+    const creatingNewPost = await createPost(
+      oldPost.title,
+      oldPost.body,
+      oldPost.media
+    );
+    expect(creatingNewPost).toEqual(newPost);
+  });
+});
