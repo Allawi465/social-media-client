b940ea6c12f5803afc8699abc11d46bd67c54c35
Mohammed <allawi465@gmail.com>
2022-11-05 03:31:19 +0100

    unit test done for login

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -23,25 +23,27 @@ function fetchFailureLogin(
   });
 }
 
+const password = '12345678';
+const mail = 'test@stud.noroff.no';
+
 const data = {
-  mail: 'test@stud.noroff.no',
-  password: '12345678',
+  mail,
   id: 1,
   token: 'valid token',
 };
 
 describe('login', () => {
-  /*   it('should return false when provided with an invalid mail or password', async () => {
+  it('should trow error when provided with an invalid mail or password', async () => {
     global.fetch = jest.fn(() => fetchFailureLogin());
     const loginFun = await login();
-    expect(loginFun).toEqual(false);
-  }); */
+    expect(loginFun).toThrow('Invalid email or password');
+  });
 
   it('Returns a valid token when provided with a valid mail and password', async () => {
+    expect(data.mail).toMatch(/[\w\-.]+@(stud.)?noroff.no$/);
+    expect(password).toMatch(/[^ ]{8,16}/);
     global.fetch = jest.fn(() => fetchSuccessLogin());
-    const loginFun = await login(data);
-    expect(loginFun.mail).toMatch(/[\w\-.]+@(stud.)?noroff.no$/);
-    expect(loginFun.password).toMatch(/[^ ]{8,16}/);
-    expect(loginFun.token).toBe('valid token');
+    const loginFun = await login(mail, password);
+    expect(loginFun.token).toEqual(data.token);
   });
 });
