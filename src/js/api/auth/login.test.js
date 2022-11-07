0b2fab57d7d3db713cf5e2a4fadd3934ab0b0cb9
Mohammed <allawi465@gmail.com>
2022-11-07 03:17:04 +0100

    adding .rejects to corret the trow error

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -25,6 +25,8 @@ function fetchFailureLogin(
 
 const password = '12345678';
 const mail = 'test@stud.noroff.no';
+const invalidPassword = '1234567';
+const invalidMail = 'test@stud2.noroff.no';
 
 const data = {
   mail,
@@ -35,12 +37,13 @@ const data = {
 describe('login', () => {
   it('should trow error when provided with an invalid mail or password', async () => {
     global.fetch = jest.fn(() => fetchFailureLogin());
-    const loginFun = await login();
-    expect(loginFun).toThrow('Invalid email or password');
+    await expect(login(invalidMail, invalidPassword)).rejects.toThrow(
+      'Invalid email or password'
+    );
   });
 
   it('Returns a valid token when provided with a valid mail and password', async () => {
-    expect(data.mail).toMatch(/[\w\-.]+@(stud.)?noroff.no$/);
+    expect(mail).toMatch(/[\w\-.]+@(stud.)?noroff.no$/);
     expect(password).toMatch(/[^ ]{8,16}/);
     global.fetch = jest.fn(() => fetchSuccessLogin());
     const loginFun = await login(mail, password);
