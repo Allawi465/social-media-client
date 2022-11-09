8bdacb0754a472b1df12db0dec4a92c3dea8d039
Mohammed <allawi465@gmail.com>
2022-11-09 23:26:37 +0100

    uppdating cypress local url

--- cypress.config.js
+++ cypress.config.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      URL: "http://127.0.0.1:5500/"
       config.env = {
+        URL: 'http://127.0.0.1:5500/',
         ...process.env,
         ...config.env,
-      }
+      };
       return config;
-    },  
+    },
   },
 });
