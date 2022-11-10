3370e3df4bba7c9fe21b2b5fdb4741372f12bc3d
Mohammed <allawi465@gmail.com>
2022-11-10 01:50:41 +0100

    deleting un use file in cypress folder

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,17 +1,15 @@
 describe('Social media app', () => {
   const password = Cypress.env('password');
 
-/*   beforeEach(() => {
+  beforeEach(() => {
     cy.visit('/');
     cy.clearLocalStorage();
-  }); */
+  });
 
   it('login with valid email and password', () => {
     cy.visit('/');
-   /*  cy.wait(1000);
-    cy.get("form#registerForm > div.modal-header > button[type='button']")
-      .should('have.class', 'btn-close')
-      .click({ force: true });
+    cy.wait(1000);
+    cy.get("button[type='reset']:visible").click();
     cy.wait(500);
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1000);
@@ -21,34 +19,35 @@ describe('Social media app', () => {
     cy.get("input[type='password']:visible")
       .should('exist')
       .type(`${password}`);
-    cy.get('.btn-success:visible').click();
+    cy.get("button[type='submit']:visible").click();
     cy.wait(1000);
     cy.then(
       () => expect(window.localStorage.getItem('profile')).to.not.be.null
     );
     cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
-    cy.url().should('include', 'profile'); */
+    cy.url().should('include', 'profile');
   });
 
-  it('login with invalid email and password', () => {
+  it('get alert when login with invalid email and password', () => {
     cy.visit('/');
-  /*   cy.wait(1000);
-    cy.get("form#registerForm > div.modal-header > button[type='button']")
-      .should('have.class', 'btn-close')
-      .click({ force: true });
+    cy.wait(1000);
+    cy.get("button[type='reset']:visible").click();
     cy.wait(500);
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1000);
     cy.get("input[type='email']:visible")
       .should('exist')
-      .type(`test@stud.noroff.no`);
-    cy.get("input[type='password']:visible").should('exist').type(`test`);
-    cy.get('.btn-success:visible').click();
-    cy.wait(1000);
-    cy.then(
-      () => expect(window.localStorage.getItem('profile')).to.not.be.null
-    );
-    cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
-    cy.url().should('include', 'profile'); */
+      .type(`test@2stud.noroff.no`);
+    cy.get("input[type='password']:visible").should('exist').type(`test123`);
+    cy.get("button[type='submit']:visible").click();
+    cy.wait(2000);
+    cy.on('window:alert', (test) => {
+      expect(test).to.equal(
+        'Either your username was not found or your password is incorrect'
+      );
+    });
+    cy.then(() => expect(window.localStorage.getItem('profile')).to.be.null);
+    cy.then(() => expect(window.localStorage.getItem('token')).to.be.null);
+    cy.url().should('not.include', 'profile');
   });
 });
