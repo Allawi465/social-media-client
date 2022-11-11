7fe8e686cecb692a1b80aaa396cffe2311a1ce31
Mohammed <allawi465@gmail.com>
2022-11-11 02:04:13 +0100

    adding screenshot folder and videos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,4 +1,5 @@
 describe('Social media app', () => {
+  const email = Cypress.env('email');
   const password = Cypress.env('password');
 
   beforeEach(() => {
@@ -13,14 +14,12 @@ describe('Social media app', () => {
     cy.wait(500);
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1000);
-    cy.get("input[type='email']:visible")
-      .should('exist')
-      .type(`MohAll71237@stud.noroff.no`);
+    cy.get("input[type='email']:visible").should('exist').type(`${email}`);
     cy.get("input[type='password']:visible")
       .should('exist')
       .type(`${password}`);
     cy.get("button[type='submit']:visible").click();
-    cy.wait(1000);
+    cy.wait(2000);
     cy.then(
       () => expect(window.localStorage.getItem('profile')).to.not.be.null
     );
