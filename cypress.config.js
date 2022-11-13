cd2c79c68e4715450f8b9c532ff062d161280cba
Mohammed <allawi465@gmail.com>
2022-11-13 21:46:21 +0100

    updating port

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,12 @@ const { defineConfig } = require('cypress');
 require('dotenv').config();
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
   e2e: {
+    video: false,
     setupNodeEvents(on, config) {
       config.env = {
-        URL: 'http://127.0.0.1:8080/',
+        URL: 'http://127.0.0.1:5500/',
         ...process.env,
         ...config.env,
       };
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 export default {
-  root: path.resolve(__dirname),
   server: {
-    port: 8080,
+    port: 5500,
     hot: true,
     host: '127.0.0.1',
   },
